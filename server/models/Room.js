f754db93838b43aea6047d1ea918fb7ef24afc26
Eugene <eugene.popolamov@effective-soft.com>
2022-07-02 17:31:46 +0300

    feat: create room schema

--- server/models/Room.js
+++ server/models/Room.js
@@ -2,5 +2,32 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const roomShema = new Schema({
-  
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  maxPeople: {
+    type: Number,
+    required: true,
+  },
+  roomNumbers: [{
+    number: Number,
+    unavailableDates: {
+      type: [Date]
+    }
+  }],
+},
+{
+  timestamps: true,
+},
+);
+
+export default mongoose.model('Room', roomShema);
